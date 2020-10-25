78a0783d82db3f7181afad529dde1fb0ece463e8
Sakub <sandomierzj@gmail.com>
2020-10-25 15:02:52 +0100

    Make calculating into sasins work, finish project

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
-const app = new Vue({
-    el: '#app',
-    data: {
-        userInput: 0
+Vue.component('input-field', {
+    template: `
+    <div>
+        <p class="app__number_input__label">Podaj liczbę:</p>
+        <input type="number" @input="handleInput" v-model="userInput" class="app__number_input" max="20" onKeyDown="if(this.value.length==25 && event.keyCode!=8 || event.keyCode == 69 || event.keyCode == 188) return false;">
+
+        <p class="app__result">{{sasins}} Sasin(a)</p>
+    </div>`,
+    data() {
+        return {
+            userInput: 0,
+            sasins: 0
+        };
+    },
+    methods: {
+        handleInput() {
+            const sasinValue = 70000000;
+            const regexPattern = /\.0+$/;
+            this.sasins = (this.userInput / sasinValue).toFixed(13).replace(regexPattern,'')
+        }
     }
-})
+    
+});
+
+const app = new Vue({
+    el: '#app'
+});
+
+
